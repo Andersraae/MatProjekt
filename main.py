ae5964bde172847084ec7779fd04c74e69048ea0
Andersraae <andersraae01@gmail.com>
2021-09-13 18:33:27 +0200

    Lolnemt

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Navn',
+        'title': 'Titel',
+        'content': 'Indhold',
+        'date_posted': 'Dato'
+    },{
+        'author': 'Navn2',
+        'title': 'Titel2',
+        'content': 'Indhold2',
+        'date_posted': 'Dato2'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == '__main__':
+    app.run(debug=True)
