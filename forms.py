5eaa3eea3de61cd2ada28de36f50ebda9484e870
Andersraae <andersraae01@gmail.com>
2021-09-14 09:58:26 +0200

    Oversæt/Farvetemplate

--- forms.py
+++ forms.py
@@ -3,14 +3,14 @@ from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirm_password = PasswordField('Confirm password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    username = StringField('Brugernavn', validators=[DataRequired(message="Feltet skal udfyldes"), Length(min=2, max=20, message="Skal være mellem 2 og 20 tegn")])
+    email = StringField('Email', validators=[DataRequired(message="Feltet skal udfyldes"), Email(message="Ugyldig Email")])
+    password = PasswordField('Kodeord', validators=[DataRequired(message="Feltet skal udfyldes")])
+    confirm_password = PasswordField('Gentag kodeord', validators=[DataRequired(message="Feltet skal udfyldes"), EqualTo('password', message="Skal være identisk med kodeord")])
+    submit = SubmitField('Opret konto')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember = BooleanField('Remember Me')
+    email = StringField('Email', validators=[DataRequired(message="Feltet skal udfyldes"), Email(message="Ugyldig Email")])
+    password = PasswordField('Kodeord', validators=[DataRequired(message="Feltet skal udfyldes")])
+    remember = BooleanField('Husk Mig')
     submit = SubmitField('Login')
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
+
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '5d974a87bda95d334175f5724388be6a'
@@ -25,25 +26,25 @@ def home():
 
 @app.route("/about")
 def about():
-    return render_template('about.html', title='About')
+    return render_template('about.html', title='Om Os')
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for { form.username.data }!', 'success')
+        flash(f'Din konto er nu oprettet, { form.username.data }!', 'success')
         return redirect(url_for('home'))
-    return render_template('register.html', title='Register', form=form)
+    return render_template('register.html', title='Opret Konto', form=form)
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
+            flash('Login var successfuld!', 'success')
             return redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login var ikke successfuld. Tjek din email og kodeord', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == '__main__':
